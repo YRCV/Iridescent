21cb093b01e91be2a48702e865465f2e83e08597
YRCV <yrc@njit.edu>
2026-02-23 14:28:17 -0500

    Initial Commit

---
+++ Iridescent/IridescentApp.swift
@@ -0,0 +1,17 @@
+//
+//  IridescentApp.swift
+//  Iridescent
+//
+//  Created by Yahil Corcino on 2/23/26.
+//
+
+import SwiftUI
+
+@main
+struct IridescentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
